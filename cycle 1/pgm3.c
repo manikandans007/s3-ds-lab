bb7f338d2af46c05b0571555d8cd84b590490685
youngwolf <102538684+manikandans007@users.noreply.github.com>
2022-10-17 22:38:27 +0530

    Update pgm3.c

--- cycle 1/pgm3.c
+++ cycle 1/pgm3.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
-    char str[100];
-    printf("Enter the string: ");
-    fgets(str, sizeof(str), stdin);
-
-    int freq[256];
-    for (int i = 0; i < 256; i++)
-        freq[i] = 0;
-
-    for (int i = 0; str[i] != '\0'; i++)
-        freq[str[i]]++;
-
-    for (int i = 0; i < 256; i++)
+    char s[1000];  
+    int  i,j,k,count=0,n;
+    printf("Enter  the string : ");
+    gets(s);
+    for(j=0;s[j];j++);
+	 n=j; 
+	printf(" frequency count character in string:\n");
+    for(i=0;i<n;i++)  
     {
-        if (freq[i] == 0)
-            continue;
-        if (i == '\n')
-            continue;
-        printf("'%c' - %d\n", i, freq[i]);
-    }
-
-    return 0;
+     	count=1;
+    	if(s[i])
+    	{
+ 		  for(j=i+1;j<n;j++)  
+	      {   
+	        if(s[i]==s[j])
+    	    {
+                 count++;
+                 s[j]='\0';
+	     	}
+	      }  
+	      printf(" '%c' = %d \n",s[i],count);
+         }
+	   } 
+ 	return 0;
 }
